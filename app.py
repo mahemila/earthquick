2f27dd6713ba78654f9886946efc67d0f7d5624e
mahhem <47625619+HemilaMahmoud@users.noreply.github.com>
2021-11-07 15:48:46 +0100

    aufgabenblatt_5

--- app.py
+++ app.py
@@ -13,9 +13,12 @@ fname = 'all_month.csv'
 path = os.path.join(pre, fname)
 df = pd.read_csv(path)
 
-dd_mag = [{'label':str(i), 'value':int(i)} for i in sorted(df['mag'].dropna().unique())]
+mag_list = set(int(i) for i in sorted(df['mag'].dropna().unique()))
+dd_mag = [{'label':str(int(i)), 'value':int(i)} for i in mag_list if i>=0]
 lands = list(set([i.split(', ')[0] if len(i.split(', '))==1 else i.split(', ')[1] for i in df['place']]))
 dd_land = [{'label':i, 'value':i} for i in sorted(lands)]
+#slider = {i:str(i*10) for i in range(0,11)}
+slider = {i:str(i) for i in mag_list}
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],suppress_callback_exceptions=True,
                 meta_tags=[
                     {"name": "viewport", "content": "width=device-width, initial-scale=1"}
@@ -23,40 +26,44 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],suppress_callback_ex
                 )
 
 app.layout = html.Div([
-        html.Div(style={'height':'10vh'}),
-        dbc.Row(
-             dbc.Col([
-                 dbc.Row([
-                     dbc.Col(['Magnitude ab',
-                              dcc.Dropdown(id='mag_num',
-                              options=dd_mag,
-                              value = min([x['value'] for x in dd_mag])
-                              )],
-                             width={"size": 3, "offset": 2}),
-                     #html.Br,
-                     dbc.Button('Alle Jahre anzeigen',n_clicks=0,style={'margin-left':'20px'} ),
-                    dbc.Button('Anzeige Zurücksetzen',n_clicks=0 ,style={'margin-left':'20px'})]
+        html.Div(
+            html.P('Earthquick - BOMBOM', style={'color':'white', 'padding-left':'100px', 'padding-top':'2vh'}),
+            style={'height':'5vh', 'background-color':'black', 'margin-lef':'30%'}),
+        html.Br(),
 
-                     )
-                 ,
-                 dcc.Graph(id='haupt_vis')],
-                width={"size": 8, "offset": 2})
-        ),
+        #dbc.Row([
+         #   dbc.Col([
+          #      'Choose Continent',
+           #     dbc.Select(
+            #        options=dd_mag,
+             #       value=min([x['value'] for x in dd_mag])
+              #  )
+        #    ],width={"size": 2, "offset": 4}),
+         #   dbc.Col(['Choose time frame',
+         #       dcc.RangeSlider(
+          #          min=min(slider.keys()),
+           #         max=max(slider.keys()),
+            #        marks=slider,
+             #       value=[min(slider.keys()), max(slider.keys())]
+              #  )
+            #], width={"size": 4}),
+        #]),
 
-        #html.Div(style={'height':'10vh'}),
-        #html.Br(),
 
-        dbc.Container(
-            dbc.Row([
+        dbc.Row([
+#col1
+            dbc.Col([
+
+                dbc.Row(
                     dbc.Col([
                         dbc.Row([
                             dbc.Col(['Land 1',
-                            dcc.Dropdown(
+                                dcc.Dropdown(
 
-                            id='land1',
-                            options=dd_land,
-                            value=sorted(lands)[0],
-                        )], width={"size": 4, "offset": 2},),
+                                id='land1',
+                                options=dd_land,
+                                value=sorted(lands)[0],
+                                )], width={"size": 4, "offset": 2},),
                             dbc.Col(
                                 ['Land 2',
                                 dcc.Dropdown(
@@ -65,32 +72,62 @@ app.layout = html.Div([
                                     value=sorted(lands)[1],
                                 )], width=4)
                         ]
-                    ),
+                        ),
+                        dbc.Row(
+                            dbc.Col(dcc.Graph(id='land_depth'),width={"size": 12}))
+                    ]),
+                ),
+                dbc.Row(
+                    dbc.Col(dcc.Graph(id='mag_hour'), width={"size": 12})
+                ),
 
-                            dcc.Graph(id='land_depth')], width=10, lg=6),
-                    dbc.Col([
-                        html.Div(style={'height':'8vh'}),
-                        dcc.Graph(
-                            figure=vis.mag_type_error()
-                        )], width=10,lg=6),
-                        ]
-                    ), {'width':10} #, style={'border-style': 'dotted','border-color':'red'}
-        ),
+            ],width={"size": 3}) ,
+#col2
+            dbc.Col([
+                #html.Br(),
+                #html.Div(style={'height':'10vh'}),
+                dbc.Row([
+                    #[dbc.Col([
+                     #   'Magnitude ab',
+                      #      dbc.Select(
+                       #         id='mag_num',
+                        #        options=dd_mag,
+                         #       value=min([x['value'] for x in dd_mag])
+                          #  )
+                        #],width={"size": 3, 'offset':1}),
 
-        #html.Div(style={'height':'10vh'}),
-        #html.Br(),
-        dbc.Row([
-                dbc.Col(dcc.Graph(
-                        figure=vis.mag_rms()
-                    ), width={"size": 5, "offset": 1}),
+                        dbc.Col(['Magnitude',#'Time frame',
+                            dcc.RangeSlider(
+                                id='mag_num',
+                                min=min([x['value'] for x in dd_mag]),#min(slider.keys()),
+                                max=max([x['value'] for x in dd_mag]),#max(slider.keys()),
+                                marks=slider,
+                                value=[min([x['value'] for x in dd_mag]), max([x['value'] for x in dd_mag])]
+                                #[min(slider.keys()), max(slider.keys())]
+                            )
+                        ], width={"size": 8, 'offset':1})]),
+                html.Br(),
+                dbc.Row(dbc.Col(
+                    dcc.Graph(id='haupt_vis')
+                       # figure=px.bar(x=[1,2,3], y=[10,20,30], height=900)
+                    ,width={"size": 12}))
+            ],width={"size": 6}),
+            #dbc.Col(dcc.Graph(
+             #   figure=px.bar(x=[1,2,3], y=[10,20,30], height=900)
+            #),width={"size": 6}),
 
+#col 3
+            dbc.Col([
+                html.Div(style={'height':'10vh'}),
+                #html.Br(),
+                dbc.Row(
+                    dbc.Col(dcc.Graph(id='mag_rms'), width={"size": 12})),
+                dbc.Row(
+                    dbc.Col(dcc.Graph(id='box_plot'), width={"size": 12})
+                ),
+            ])
+        ]),
 
-                dbc.Col([
-                    dcc.Graph(
-                        figure=px.scatter(x=[1,2,3], y=[10,20,30])
-                    )], width={"size": 5}),
-            ]
-        ),
 ])
 
 @app.callback(
@@ -101,16 +138,38 @@ def get_mag(mag_list):
     return vis.h_diagram(mag_list)
 
 @app.callback(
+    Output('mag_hour', 'figure'),
+    [Input('mag_num', 'value')]
+)
+def get_box_plot(mag_list):
+    return vis.hours_mags(mag_list)
+
+@app.callback(
     Output('land_depth', 'figure'),
     [Input('land1', 'value'),
-     Input('land2', 'value')]
+     Input('land2', 'value'),
+     Input('mag_num', 'value')]
 )
-def get_lands(l1,l2):
+def get_lands(l1,l2,mag_list):
     if l1=='':
         l1==sorted(lands)[0]
     if l2=='':
         l1 == sorted(lands)[1]
-    return vis.lands_vis(l1,l2)
+    return vis.lands_vis(l1,l2, mag_list)
+
+@app.callback(
+    Output('mag_rms', 'figure'),
+    [Input('mag_num', 'value')]
+)
+def get_box_plot(mag_list):
+    return vis.mag_rms(mag_list)
+
+@app.callback(
+    Output('box_plot', 'figure'),
+    [Input('mag_num', 'value')]
+)
+def get_box_plot(mag_list):
+    return vis.box_month_gab(mag_list)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- visualizations.py
+++ visualizations.py
@@ -2,6 +2,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import os
+import numpy as np
 import geopandas as gpd
 
 pre= os.path.dirname(os.path.realpath(__file__))
@@ -11,7 +12,7 @@ path = os.path.join(pre, fname)
 df = pd.read_csv(path)
 
 def h_diagram(mag_list):
-    geo_df = df[df['mag'] >= mag_list]
+    geo_df = df[(df['mag'] >= int(mag_list[0])) &  (df['mag'] <= int(mag_list[1]))]
     geo_df['Clock'] = pd.DatetimeIndex(geo_df['time']).hour
 
     geo_df = geo_df.sort_values('Clock', ascending=True)
@@ -21,14 +22,43 @@ def h_diagram(mag_list):
                          lon=geo_df['longitude'],
                          hover_name="place",
                         #animation_frame='Clock',
-                         color = 'mag'
+                         color = 'mag',
+                         width=1000, height=600
     )
     #fig.update_traces(marker={'size':5})
     return fig
 
-def lands_vis(l1, l2):
-    dff = df[df['place'].str.contains(l1)]
-    dff_1 = df[df['place'].str.contains(l2)]
+def hours_mags(mag_list):
+    df_mag_hours = df.copy()
+    df_mag_hours = df_mag_hours[(df_mag_hours['mag'] >= int(mag_list[0])) & (df['mag'] <= int(mag_list[1]))]
+    df_mag_hours['Clock'] = pd.DatetimeIndex(df_mag_hours['time']).hour
+    df_droped = df_mag_hours.dropna()
+    df_droped['mag_int'] = df_droped['mag']#.astype(int)
+    df_x = df_droped[['Clock', 'mag_int']]
+    df_x['ones'] = np.ones(len(df_x))
+    df_x_grouped = df_x.groupby('Clock').sum().reset_index()
+    df_x_grouped['mean'] = round(df_x_grouped.mag_int / df_x_grouped.ones, 2)
+
+    fig = px.line(df_x_grouped, x='Clock', y='mean', labels={'mean':'Mean Magnitude'})
+    """
+    fig = go.Figure(go.Scatter(
+        x=df_x_grouped['Clock'], y=df_x_grouped['mean'], text=df_x_grouped['mean'],
+        mode='markers+lines+text',
+        textposition='middle center',
+        textfont=dict(color='black'),
+        line=dict(color='blue'),
+        marker=dict(symbol='circle', size=30, color='white',
+                    line=dict(width=1))))
+    """
+    return fig
+
+
+
+def lands_vis(l1, l2, mag_list):
+    dff_a = df.copy()
+    dff_a = dff_a[(dff_a['mag'] >= int(mag_list[0])) & (dff_a['mag'] <= int(mag_list[1]))]
+    dff = dff_a[dff_a['place'].str.contains(l1)]
+    dff_1 = dff_a[dff_a['place'].str.contains(l2)]
     mean_1 = dff['depth'].mean()
     mean_2 = dff_1['depth'].mean()
 
@@ -50,6 +80,16 @@ def mag_type_error():
 
     return fig
 
-def mag_rms():
-    fig = px.scatter(df, x='mag', y='rms', trendline="ols", trendline_color_override="black")
+def mag_rms(mag_list):
+    df_mag_rms = df.copy()
+    df_mag_rms = df_mag_rms[(df_mag_rms['mag'] >= int(mag_list[0])) & (df['mag'] <= int(mag_list[1]))]
+    fig = px.scatter(df_mag_rms, x='mag', y='rms', trendline="ols", trendline_color_override="red")
     return fig
+def box_month_gab(mag_list):
+    df_month_gap = df.copy()
+    df_month_gap = df_month_gap[(df_month_gap['mag'] >= int(mag_list[0])) & (df['mag'] <= int(mag_list[1]))]
+    df_month_gap['Month'] = pd.DatetimeIndex(df_month_gap['time']).month
+    fig = px.box(df_month_gap, x='Month', y='gap', labels = {'Month':'Month in Year 2021'})
+
+    return fig
+
