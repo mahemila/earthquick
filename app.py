764c07678b025cceb5d1335766de12cd70bdaf50
Samuel Martin <me@samuelnmartin.ch>
2022-01-14 02:22:43 +0100

    Improved layout and design

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import dash
-from dash import html
-from dash import dcc
+from dash import html, dcc
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -29,108 +28,210 @@ lands = list(set([i.split(', ')[0] if len(i.split(', '))==1 else i.split(', ')[1
 dd_land = [{'label':i, 'value':i} for i in sorted(lands)]
 sorted_lands = sorted(lands)
 
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 
-app.layout = html.Div([
-    #top Bar
-        html.Div(
-            html.P('Earthquick - BOMBOM', style={'color':'white', 'padding-left':'100px', 'padding-top':'2vh'}),
-            style={'height':'5vh', 'background-color':'black', 'margin-lef':'30%'}),
-        html.Br(),
+#=====================
+# START PAGE LAYOUT
+#=====================
 
-    #Slider => controls all diagramms
-        dbc.Row([
-                dbc.Col(['Magnitude',
-                        dcc.RangeSlider(
-                            id='mag_num',
-                            min=min([x['value'] for x in dd_mag]),
-                            max=max([x['value'] for x in dd_mag]),
-                            marks=slider,
-                            value=[min([x['value'] for x in dd_mag]), max([x['value'] for x in dd_mag])]
-                        )
-                    ], width={"size": 4, 'offset':4}),
-                dbc.Col(dbc.Button('Informations', outline=True, color="info", className="me-1",id='infoButton', n_clicks=0),
-                        width={"size": 2, 'offset':2})
-        ]),
-    dbc.Modal([dbc.ModalHeader(html.H4('Willkommen auf dem Dashboard zur Visualisierung von Erdbebendaten')),
-               dbc.ModalBody(html.Div([
-                   html.P([dcc.Markdown('''
-                   Auf diesem Dashboard werden Daten zu Erdbeben der USGS (https://earthquake.usgs.gov/) in verschiedenen 
-                   Diagrammen visualisiert. Mithilfe einer Magnitude-Slide lassen sich die Daten in den Diagrammen 
-                   einheitlich anpassen. Im Diagramm zur Visualisierung der durchschnittlichen Erdbebentiefe pro Land 
-                   lassen sich zudem durch manuelle Anpassungen verschiedene Länder bezüglich ihrer durchschnittlichen 
-                   Erdbebentiefe vergleichen. 
-                   Wir hoffen sehr, mit diesem Dashboard einen interessanten Einblick in die Welt der Seismologie geben zu können.
-                   ''')])
+#navbar
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                dbc.Row(
+                    [
+                        #dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Earthquick", className="ms-2")),
+                    ],
+                    align="left",
+                    className="g-0",
+                ),
+            ),
+        ],
+    fluid=True
+    ),
+    color="dark",
+    dark=True,
+) # close navebar
 
-               ])),
+#Slider with modal button => slider controls all diagramms
+slider = dbc.Row(
+    [
+
+    # Slider
+    dbc.Col(['Magnitude',
+        dcc.RangeSlider(
+            id='mag_num',
+            min=min([x['value'] for x in dd_mag]),
+            max=max([x['value'] for x in dd_mag]),
+            marks=slider,
+            value=[min([x['value'] for x in dd_mag]), max([x['value'] for x in dd_mag])]
+        )
+    ], width={"size": 4, 'offset':4}),
+
+    # Button
+    dbc.Col(
+        dbc.Button(
+            'Informations',
+            outline=False,
+            color="primary",
+            className="me-1",
+            id='infoButton',
+            n_clicks=0),
+    className="d-grid gap-2 pr-2 row align-items-center",
+    ),
+
+    # modal contents
+    dbc.Modal(
+        [
+            dbc.ModalHeader(
+                html.H4('Willkommen auf dem Dashboard zur Visualisierung von Erdbebendaten')
+            ),
+
+            dbc.ModalBody(html.Div([
+                html.P([
+                    dcc.Markdown('''
+                    Auf diesem Dashboard werden Daten zu Erdbeben der USGS (https://earthquake.usgs.gov/) in verschiedenen 
+                    Diagrammen visualisiert. Mithilfe einer Magnitude-Slide lassen sich die Daten in den Diagrammen 
+                    einheitlich anpassen. Im Diagramm zur Visualisierung der durchschnittlichen Erdbebentiefe pro Land 
+                    lassen sich zudem durch manuelle Anpassungen verschiedene Länder bezüglich ihrer durchschnittlichen 
+                    Erdbebentiefe vergleichen. 
+                    Wir hoffen sehr, mit diesem Dashboard einen interessanten Einblick in die Welt der Seismologie geben zu können.
+                    ''')])
+
+            ])),
+
             dbc.ModalFooter(
-                            [html.P('Developed by I.Kilchenmann, S.Weber-Martin and M.Hemila 2021-2022',
-                                    style={'margin-left':0, 'margin-right':'auto'}),
-                                html.P(''),
-                                dbc.Button(
-                                "Close", id="close", className="ms-auto", n_clicks=0
-                            )]
-                        )
+                [html.P(
+                    'Developed by I.Kilchenmann, S.Weber-Martin and M.Hemila 2021-2022',
+                    style={'margin-left':0, 'margin-right':'auto'}),
+                    html.P(''),
+                    dbc.Button(
+                        "Close", id="close", className="ms-auto", n_clicks=0
+                    )] # close html.p
+            ) # close footer
 
-               ],
-                id="modal",
-                scrollable=True,
-                is_open=False,
-                size="lg"
-),
-    #=====================
-    # START DIAGRAMMS
-    # Structure:
-        # 1 Row => 3 Columns
-        # Col 1,3 => 2 Rows
-        # Col 2 => 1 Row
-    #=====================
+        ],
 
-        dbc.Row([
-#First column
-            dbc.Col([
-                dbc.Row(
-                    dbc.Col([
-                        dbc.Row([
-                            dbc.Col(['Choose Regions',
-                                dcc.Dropdown(
-                                id='lands',
-                                options=dd_land,
-                                value=[sorted_lands[0],sorted_lands[1]],
-                                multi=True
-                                )], width={"size": 8, "offset": 2})
-                        ]),
-                        dbc.Row(
-                            dbc.Col(dcc.Graph(id='land_depth',config={'displayModeBar': False}),width={"size": 12}))
-                    ]),
-                ),
-                dbc.Row(
-                    dbc.Col(dcc.Graph(id='mag_hour'), width={"size": 12})
-                ),
+        # modal options
+        id="modal",
+        scrollable=True,
+        is_open=False,
+        size="lg"
 
-            ],width={"size": 3}),
+    ) # close modal
 
-#Seconed column: Main diagramm
-            dbc.Col([
-                html.Div(style={'height':'12vh'}),
-                dbc.Row(dbc.Col(
-                    dcc.Graph(id='haupt_vis')
-                    ,width={"size": 10}))
-            ],width={"size": 5}),
+],
+#options slider column
+style={'background-color':'grey', 'margin-right':0},
+className='py-3 mb-3'
+) # close
 
-#Third column
-            dbc.Col([
-                html.Div(style={'height':'10vh'}),
-                dbc.Row([#'Zusammenhang zwischen Erdbeben und Magnitude',
-                    dbc.Col(dcc.Graph(id='mag_rms'), width={"size": 12})]),
-                dbc.Row(
-                    dbc.Col(dcc.Graph(id='box_plot'), width={"size": 12})
-                ),
-            ], width={"size": 3, "offset": 1})
-        ]),
 
+#=====================
+# START DIAGRAMMS
+#=====================
+
+# diagramm mag hour
+mag_hour = dbc.Row(
+    dbc.Col(
+        dcc.Graph(
+            id='mag_hour'
+        ),
+        width={"size": 12}
+    )
+)
+
+# diagramm regions
+regions = dbc.Col([
+
+    dbc.Row([
+        dbc.Col(['Choose Regions',
+            dcc.Dropdown(
+                id='lands',
+                options=dd_land,
+                value=[sorted_lands[0],sorted_lands[1]],
+                multi=True
+            )],
+        width={"size": 8, "offset": 2}
+        )
+    ]),
+
+    dbc.Row(
+        dbc.Col(
+            dcc.Graph(
+                id='land_depth',
+                config={'displayModeBar': False}
+            ),
+        width={"size": 12})
+    )
 ])
+
+#diagramm map
+map = dcc.Graph(id='haupt_vis')
+
+# diagramm rms
+rms=dcc.Graph(id='mag_rms')
+
+#diagramm box_plot
+box_plot = dcc.Graph(id='box_plot')
+
+
+
+# layout for diagramms
+visualisation = html.Div(
+    [
+        dbc.Row([
+            dbc.Col( # First column
+                [
+                    dbc.Col(
+                        [regions],
+                        className='bg.light shadow p-3 mb-5 bg-body rounded'
+                    ),
+
+                    dbc.Col(
+                        [mag_hour],
+                        className='bg.light shadow p-3 mb-5 bg-body rounded'
+                        )
+                ],
+                className='col-3'
+
+            ), # End first column
+
+            dbc.Col( # start second column
+                dbc.Col(
+                    [map],
+                    className='bg.light shadow p-3 mb-5 bg-body rounded',
+                )
+            ),
+
+            dbc.Col( # start third column
+                [
+                    dbc.Col(
+                        [rms],
+                        className='bg.light shadow p-3 mb-5 bg-body rounded'
+                    ),
+
+                    dbc.Col(
+                        [box_plot],
+                        className='bg.light shadow p-3 mb-5 bg-body rounded'
+                    )
+                ],
+                className='col-3'
+            ),  # End third column
+
+        ]), # Row end
+
+    ]
+)
+
+# building app layout
+app.layout = html.Div([navbar, slider, visualisation])
+
 #========================================
 # End of App-Layout / Start of Callbacks
 #========================================
@@ -149,7 +250,6 @@ def get_figures(mag_list):
            vis.mag_rms(ff_df),\
            vis.box_month_gab(ff_df)
 
-
 @app.callback(
     Output('land_depth', 'figure'),
     [Input('lands', 'value'),
@@ -167,7 +267,6 @@ def toggle_info(n1, n2, is_open):
     if n1 or n2:
         return not is_open
     return is_open
-
 
 #=============
 #End Callbacks
